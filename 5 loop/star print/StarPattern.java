f1635b7e9ffd6dad4813ba9e4d74d0f461bd6f34
Ajay Singh <ajaysingh8xx@gmail.com>
2026-07-24 21:56:02 +0530

    starPattern some changes

--- 5 loop/star print/StarPattern.java
+++ 5 loop/star print/StarPattern.java
@@ -1,13 +1,21 @@
+// Class ka naam
 public class StarPattern {
+
+    // Main method, yahin se program start hota hai
     public static void main(String args[]) {
+        // Total rows ki value
         int n = 4;
+
+        // Outer loop rows ko control karta hai
         for (int i = 0; i <= n; i++) {
 
-            System.out.println(" ");
+            // Inner loop har row me stars print karta hai
             for (int j = 0; j < i; j++) {
+                // Ek star print karo, next line me nahi ja raha hai
                 System.out.print("*");
             }
-
+            // Current row complete hone ke baad next line me ja raha hai
+            System.out.println();
         }
     }
 }
