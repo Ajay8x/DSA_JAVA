81ea5016267ad8cbb4724bde716a83190e8ae6c8
Ajay Singh <ajaysingh8xx@gmail.com>
2026-07-24 22:59:50 +0530

    function

--- 7 Function_Methods/1_printHello_world_function/printHello_world_function.java
+++ 7 Function_Methods/1_printHello_world_function/printHello_world_function.java
@@ -4,9 +4,7 @@ public class printHello_world_function {
     // function
     public static void printHello_world_function() {
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+   
         return;
 
     }
