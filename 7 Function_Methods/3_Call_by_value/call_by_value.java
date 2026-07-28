351e31e304c7f281ddc4e02ced8802d894a1eb3c
Ajay Singh <ajaysingh8xx@gmail.com>
2026-07-28 13:54:38 +0530

    call by value with swap function

---
+++ 7 Function_Methods/3_Call_by_value/call_by_value.java
@@ -0,0 +1,19 @@
+
+public class call_by_value {
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Inside swap method: a = " + a + ", b = " + b);
+    }
+    
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("Before swap: x = " + x + ", y = " + y);
+        swap(x, y);
+        System.out.println("After swap: x = " + x + ", y = " + y);
+    }   
+}
