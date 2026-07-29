92cd2aec0f5bfa0f6ad698369fbd2226e2ff5702
Ajay Singh <ajaysingh8xx@gmail.com>
2026-07-29 13:51:56 +0530

    BINtoDEC method implementation

--- 7 Function_Methods/BinToDec/BinToDec.java
+++ 7 Function_Methods/BinToDec/BinToDec.java
@@ -1,8 +1,21 @@
-package BinToDec;
-
 public class BinToDec {
 
+    public static void BINtoDEC(int binNum) {
+        int power = 0;
+        int decNum = 0;
 
-    
-    
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum += lastDigit * (int) Math.pow(2, power);
+            binNum /= 10;
+            power++;
+        }
+
+        System.out.println("Decimal Number: " + decNum);
+    }
+
+    public static void main(String[] args) {
+        int binNum = 111;
+        BINtoDEC(binNum);
+    }
 }
