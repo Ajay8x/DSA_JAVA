2b1d0ed62781b9532a5d578cba88233b797a4abc
ajay8x <ajaysingh8xx@gmail.com>
2026-08-05 13:00:39 +0530

    Refactor Array_Input_Output_Update to streamline input/output and remove unused code

--- 9 Arrays Part_1/2_Array_Input_Outpu_Update/Array_Input_Output_Update.java
+++ 9 Arrays Part_1/2_Array_Input_Outpu_Update/Array_Input_Output_Update.java
@@ -5,65 +5,48 @@ import java.util.Scanner;
 
   public static void main(String[] args) { 
 
-  Scanner scanner = new Scanner(System.in);
+  Scanner sc = new Scanner(System.in);
 //         // Input the size of the array
 int marks[]=new int[100];
+// Input marks for 4 subjects
  marks[0]= sc.nextInt();  //physics
  marks[1]= sc.nextInt();  //chemistry
  marks[2]= sc.nextInt();  //maths
  marks[3]= sc.nextInt();  //english
+// Output the marks
+System.out.println("Marks in Physics: " + marks[0]);
+System.out.println("Marks in Chemistry: " + marks[1]);  
+System.out.println("Marks in Maths: " + marks[2]);
+System.out.println("Marks in English: " + marks[3]);
 
 
 
+// Update marks for a subject
+marks[2] = 95; // Update Maths marks to 95
 
+System.out.println("Updated Marks in Maths: " + marks[2]);
 
+int totalMarks = marks[0] + marks[1] + marks[2] + marks[3];
+System.out.println("Total Marks: " + totalMarks);
 
-
-
-
+int percentage = (totalMarks / 4);
+System.out.println("Percentage: " + percentage + "%");
 
   }
-   
+}
 
 
-//         System.out.print("Enter the size of the array: ");
-//         int size = scanner.nextInt();
 
-//         // Create an array of the specified size
-//         int[] array = new int[size];
 
-//         // Input elements into the array
-//         System.out.println("Enter " + size + " elements:");
-//         for (int i = 0; i < size; i++) {
-//             array[i] = scanner.nextInt();
-//         }
 
-//         // Output the original array
-//         System.out.println("Original array:");
-//         for (int i = 0; i < size; i++) {
-//             System.out.print(array[i] + " ");
-//         }
-//         System.out.println();
 
-//         // Update an element in the array
-//         System.out.print("Enter the index of the element to update (0 to " + (size - 1) + "): ");
-//         int indexToUpdate = scanner.nextInt();
-        
-//         if (indexToUpdate >= 0 && indexToUpdate < size) {
-//             System.out.print("Enter the new value: ");
-//             int newValue = scanner.nextInt();
-//             array[indexToUpdate] = newValue;
 
-//             // Output the updated array
-//             System.out.println("Updated array:");
-//             for (int i = 0; i < size; i++) {
-//                 System.out.print(array[i] + " ");
-//             }
-//             System.out.println();
-//         } else {
-//             System.out.println("Invalid index. No update performed.");
-//         }
 
-//         scanner.close();
-  
-}  
+
+
+
+
+
+
+
+
