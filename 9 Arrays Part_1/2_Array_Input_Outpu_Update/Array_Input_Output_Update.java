8414d198d6e555610fd62ed4df86f8dd7a0ff3f5
ajay8x <ajaysingh8xx@gmail.com>
2026-08-05 12:48:43 +0530

    creating type of arrays

---
+++ 9 Arrays Part_1/2_Array_Input_Outpu_Update/Array_Input_Output_Update.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+
+ public class Array_Input_Output_Update {  
+
+  public static void main(String[] args) { 
+
+  Scanner scanner = new Scanner(System.in);
+//         // Input the size of the array
+int marks[]=new int[100];
+ marks[0]= sc.nextInt();  //physics
+ marks[1]= sc.nextInt();  //chemistry
+ marks[2]= sc.nextInt();  //maths
+ marks[3]= sc.nextInt();  //english
+
+
+
+
+
+
+
+
+
+
+  }
+   
+
+
+//         System.out.print("Enter the size of the array: ");
+//         int size = scanner.nextInt();
+
+//         // Create an array of the specified size
+//         int[] array = new int[size];
+
+//         // Input elements into the array
+//         System.out.println("Enter " + size + " elements:");
+//         for (int i = 0; i < size; i++) {
+//             array[i] = scanner.nextInt();
+//         }
+
+//         // Output the original array
+//         System.out.println("Original array:");
+//         for (int i = 0; i < size; i++) {
+//             System.out.print(array[i] + " ");
+//         }
+//         System.out.println();
+
+//         // Update an element in the array
+//         System.out.print("Enter the index of the element to update (0 to " + (size - 1) + "): ");
+//         int indexToUpdate = scanner.nextInt();
+        
+//         if (indexToUpdate >= 0 && indexToUpdate < size) {
+//             System.out.print("Enter the new value: ");
+//             int newValue = scanner.nextInt();
+//             array[indexToUpdate] = newValue;
+
+//             // Output the updated array
+//             System.out.println("Updated array:");
+//             for (int i = 0; i < size; i++) {
+//                 System.out.print(array[i] + " ");
+//             }
+//             System.out.println();
+//         } else {
+//             System.out.println("Invalid index. No update performed.");
+//         }
+
+//         scanner.close();
+  
+}  
